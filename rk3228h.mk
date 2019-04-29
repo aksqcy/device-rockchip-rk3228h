24685c33617a42387aa2dfd6c897f2a9c14f34e5
Xiao Ya peng <yp.xiao@rock-chips.com>
2019-04-29 16:31:10 +0800

    rk3228h.mk: fix email apk flash back
    Signed-off-by: Xiao Ya peng <yp.xiao@rock-chips.com>

--- rk3228h.mk
+++ rk3228h.mk
@@ -37,6 +37,6 @@ PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/spn-conf.xml:system/etc/s
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.version = 1.0.0 \
     ro.product.ota.host = www.rockchip.com:2300 \
-    ro.sf.lcd_density=160
+    ro.sf.lcd_density=230
 
 PRODUCT_HAVE_OPTEE := true
